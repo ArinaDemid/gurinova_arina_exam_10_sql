58eb06b3949932a447c422c9aa92b2b252d2feee
Arina Gurinova <arina.gurinova@gmail.com>
2020-02-22 11:44:56 +0600

    Прописала Select для routs в comment.js и news.js

--- news-api/app/comments.js
+++ news-api/app/comments.js
@@ -3,7 +3,14 @@ const mysqlDB = require("../mysqlDB");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  const comments = await mysqlDB.getConnection().query('SELECT * FROM `comments`');
 
+  if (!req.query.news_id) {
+    res.send(comments);
+  } else {
+    const commentsWithQueryID = await mysqlDB.getConnection().query('SELECT * FROM `comments` WHERE `id_new` = ?', req.query.news_id);
+    res.send(commentsWithQueryID);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
--- news-api/app/news.js
+++ news-api/app/news.js
@@ -20,11 +20,19 @@ const upload = multer({ storage });
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  
+  const news = await mysqlDB.getConnection().query('SELECT `id`, `title`, `image`, `datetime` FROM `news`');
+  res.send(news);
 });
 
 router.get("/:id", async (req, res) => {
+  const newOne = await mysqlDB.getConnection().query('SELECT * FROM `news` WHERE `id` = ?', req.params.id);
   
+  const newElement = newOne[0];
+  if(!newElement) {
+    return res.status(404).send({error: 'ID not found'});
+  }
+
+  res.send(newElement);
 });
 
 router.delete("/:id", async (req, res) => {
