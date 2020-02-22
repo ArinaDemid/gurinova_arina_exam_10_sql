94abdada6efe543b7273223d4d7ebdff1960ea60
Arina Gurinova <arina.gurinova@gmail.com>
2020-02-22 11:59:55 +0600

    Прописала INSERT  для router в news.js(без тестирования)

--- news-api/app/news.js
+++ news-api/app/news.js
@@ -41,7 +41,28 @@ router.delete("/:id", async (req, res) => {
 });
 
 router.post("/", upload.single("image"), async (req, res) => {
+  const newData = req.body;
+  const datetime = new Date().toISOString();
+  
+  if (req.file) {
+    req.body.image = req.file.filename;
+  }
 
+  if (!req.body.title) {
+    res.status(400).send({ error: "Title must be present in the request" });
+  }
+
+  if (!req.body.content) {
+    res.status(400).send({ error: "Content must be present in the request" });
+  }
+
+  await mysqlDB.getConnection().query(
+    'INSERT INTO `news` (`title`, `content`, `image`, `datetime`) VALUES' + 
+    '(?, ?, ?, ?)',
+    [newData.title, newData.content, newData.image, datetime]
+  );
+
+  res.send(req.body);
 });
 
 module.exports = router;
