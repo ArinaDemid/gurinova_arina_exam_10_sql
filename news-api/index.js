43136365519fd6fa2b7d6a120462bab56d2deff7
Arina Gurinova <arina.gurinova@gmail.com>
2020-02-22 11:27:10 +0600

    Добавила структуру в api

---
+++ news-api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+// const news = require("./app/news");
+// const comments = require("./app/comments");
+const mysqlDB = require('./mysqlDB');
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+// app.use("/news", news);
+// app.use("/comments", comments);
+
+const run = async () => {
+  await mysqlDB.connect();
+
+  app.listen(port, () => {
+    console.log(`HTTP Server started on${port} port!`);
+  });
+
+  process.on('exit', () => {
+    mysqlDB.disconnect();
+  });
+};
+
+run().catch(e => {
+  console.log(e);
+});
