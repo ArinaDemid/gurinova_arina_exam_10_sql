d46d6e081379bba917e135422a2335c6c913eea7
Arina Gurinova <arina.gurinova@gmail.com>
2020-02-22 11:34:34 +0600

    Добавила структуру в api для роута по Comments

---
+++ news-api/app/comments.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mysqlDB = require("../mysqlDB");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+
+});
+
+router.delete("/:id", async (req, res) => {
+
+});
+
+router.post("/", async (req, res) => {
+
+});
+
+module.exports = router;
